ad0756dd9140641f6ab8410852603f01d51418b7
Andres Cuevas <testo_acg@hotmail.com>
2023-05-22 19:08:51 -0600

    ftr:Agregando favicon y logo

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./header.css";
 import CTA from "./CTA";
+import LOGO from "../../assets/acgLogo.png";
 import ME from "../../assets/me.png";
 import HeaderSocials from "./HeaderSocials";
 
@@ -8,6 +9,7 @@ const Header = () => {
   return (
     <header>
       <section id="home" className="container header_container">
+        <img src={LOGO} alt="logo acgcode" className="me_logo" />
         <h5>Hello I´m</h5>
         <h1>Andres Cuevas</h1>
         <h5 className="text-light">FullStack Developer</h5>
@@ -16,7 +18,7 @@ const Header = () => {
         <HeaderSocials />
 
         <div className="me">
-          <img src={ME} alt="me" />
+          <img src={ME} alt="me" className="me_img" />
         </div>
 
         <a href="#contact" className="scroll_down">
